ef475247a57f3a549b41f5f25a049ccdbf8a87c5
Muhammad Waqas Qamar <116387941+BloggerSol@users.noreply.github.com>
2026-03-01 02:45:15 +0500

    Add Sitemap.js for generating a blog sitemap

---
+++ JavaScripts/Sitemap.js
@@ -0,0 +1,56 @@
+var labelNames = {
+  'Android': 'Android Tools',
+  'Dongles': 'Box/Dongles',
+  'Flash': 'Flash Tools',
+  'FRP': 'FRP Tools',
+  'Hardware': 'Hardware Tools',
+  'iCloud': 'iCloud Tools'
+  'Drivers': 'USB Drivers'
+};
+
+function bloggersitemap(data) {
+  var entries = data.feed.entry || [];
+  var posts = [];
+
+  for (var i = 0; i < entries.length; i++) {
+    var entry = entries[i];
+    var title = entry.title.$t;
+    var url = '';
+
+    for (var j = 0; j < entry.link.length; j++) {
+      if (entry.link[j].rel === 'alternate') {
+        url = entry.link[j].href;
+        break;
+      }
+    }
+
+    if (entry.category) {
+      for (var k = 0; k < entry.category.length; k++) {
+        var label = entry.category[k].term.split(';')[0];
+        posts.push({ title: title, url: url, label: label });
+      }
+    }
+  }
+
+  posts.sort(function(a, b) {
+    if (a.label !== b.label) return a.label.localeCompare(b.label);
+    return a.title.localeCompare(b.title);
+  });
+
+  var html = '';
+  var currentLabel = '';
+
+  for (var i = 0; i < posts.length; i++) {
+    if (posts[i].label !== currentLabel) {
+      if (currentLabel) html += '</div></div>';
+      currentLabel = posts[i].label;
+      var heading = labelNames[currentLabel] || currentLabel;
+      html += '<div class="section"><h2>' + heading + '</h2><div class="grid">';
+    }
+    html += '<p><a href="' + posts[i].url + '">' + posts[i].title + '</a></p>';
+  }
+
+  if (currentLabel) html += '</div></div>';
+
+  document.getElementById('sitemap').innerHTML = html;
+}
